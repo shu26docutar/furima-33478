d39f50f72e2eb61e510f353e62ec616c997c8f6f
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-01-08 10:08:44 +0900

    ユーザー機能実装の準備（rspec/devise/factory/faker）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 class ApplicationController < ActionController::Base
 
-    before_action :basic_auth
+#     before_action :basic_auth 一時的機能の停止
 
-    private
-    def basic_auth
-        authenticate_or_request_with_http_basic do |username, password|
-            username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-        end
-    end
-end
+#     private
+#     def basic_auth
+#         authenticate_or_request_with_http_basic do |username, password|
+#             username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+#         end
+#     end
+# end
 
+
+# パスワードのメモ
 # name: shuhei
 # pass: 0926
