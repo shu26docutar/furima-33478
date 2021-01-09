0d3d8e92f74b2d5f6ce40fe54d26e9885719953c
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-01-09 22:52:20 +0900

    ユーザー機能・バックエンド実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
 #     before_action :basic_auth 一時的機能の停止
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
 #     private
 #     def basic_auth
@@ -8,7 +9,11 @@ class ApplicationController < ActionController::Base
 #             username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
 #         end
 #     end
-# end
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    end
+    
+end
 
 
 # パスワードのメモ
---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+        devise  :database_authenticatable, :registerable,
+                :recoverable, :rememberable, :validatable
+
+        has_many :items
+        has_many :purchase
+
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+        end
+
+        with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナを使用してください' } do
+          validates :first_name_kana
+          validates :last_name_kana
+        end
+
+        validates :nickname, presence: true
+        validates :password, presence: true, length: { minimum:6 }, format: { with: /\A[a-z0-9]+\z/}
+        validates :birthday, presence: true
+end
+# , message: '半角英数字を使用してください' 
