b129a4908d40f3908d3c55e44829348a9f784286
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-01-13 17:17:15 +0900

    Robocopで整形実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :item do
-    title           {Faker::Lorem.sentence}
-    description     {Faker::Lorem.sentence}
-    item_price      {301}
-    prefecture_id   {1}
-    shipping_fee_id {1}
-    shipping_day_id {1}
-    category_id     {1}
-    status_id       {1}
+    title           { Faker::Lorem.sentence }
+    description     { Faker::Lorem.sentence }
+    item_price      { 301 }
+    prefecture_id   { 1 }
+    shipping_fee_id { 1 }
+    shipping_day_id { 1 }
+    category_id     { 1 }
+    status_id       { 1 }
 
     association :user
-    
+
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/Railsの処理の流れのコピー.png'), filename: 'test_image_png')
     end
