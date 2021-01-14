55918800cb63715393a23d6f765b4af82294fdd5
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-01-14 13:33:12 +0900

    編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to item_path(item.id)
+      redirect_to item_path(@item.id)
     else
       render :new
     end
@@ -24,18 +24,18 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    # unless @item.user_id == current_user.id
-    #   redirect_to action: :index
-    # end
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
   
   def update
-    # item = Item.find(params[:id])
-    # if item.update(item_params)
-    #   redirect_to item_path(item.id)
-    # else
-    #   render :edit
-    # end
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
