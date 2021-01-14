0a3e0e0ba1ce97590f37d70119d8bedf698d5d63
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-01-14 11:31:59 +0900

    詳細機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :action_params, only: [:show, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -12,7 +13,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path(@item)
+      redirect_to item_path(item.id)
     else
       render :new
     end
@@ -23,23 +24,28 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
   
   def update
     item = Item.find(params[:id])
     if item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path(item.id)
     else
       render :edit
     end
   end
-  
 
   private
 
   def item_params
     params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id,
                                  :shipping_day_id, :item_price).merge(user_id: current_user.id)
+  end
+
+  def action_params
+    @item = Item.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,14 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchase
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name
-    validates :last_name
-  end
+    validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
 
-  with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナを使用してください' } do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
+    validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナを使用してください' }
+    validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナを使用してください' }
 
-  with_options presence: true do
-    validates :nickname
-    validates :birthday
-  end
+    validates :nickname, presence: true 
+    validates :birthday, presence: true 
 
   validates :password, presence: true, length: { minimum: 6 },
                        format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: 'は半角英数字で入力してください' }
